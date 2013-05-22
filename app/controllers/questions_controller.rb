c10353484035efcffa5c77a60bd595f94534f009
jeroenrietveld <jeroen_rietveld13@msn.com>
2013-05-22 23:33:01 +0200

    added functionality to submit questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,4 +6,20 @@ class QuestionsController < ApplicationController
   def new
   	@question = Question.new
   end
+
+  def create
+  	@question = Question.new(q_params)
+
+  	if @question.save
+  		#saved
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+
+  	def q_params
+  		params.require(:question).permit(:title, :question)
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Oldtimers::Application.routes.draw do
   resources :questions
 
-  get "questions/new"
   root to: 'default_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
